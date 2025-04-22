361adf31191f9ebade1e6f8933c56a02c7332cf1
amrsk468 <zakaramit09@gmail.com>
2025-04-22 16:02:14 +0300

    adding accounts tab buttons and labels

--- main.py
+++ main.py
@@ -5,7 +5,56 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg as tkAgg
 import datetime as dt
+import json
 
+def save_accounts():
+    with open("accounts.json", "w") as f:
+        json.dump(accounts, f)
+
+def print_account_data(win: Frame, items: list = []):
+    for i in items:
+        i.destroy()
+    account_labels = []
+    account_detail_btn = []
+    account_transaction_btn = []
+    account_del_btn = []
+    i = 0
+    for account in accounts.items():
+        account_labels.append(ttk.Label(win, text=f"{account[0]}: ${account[1]}"))
+        account_labels[i].grid(row=i + 1, column=0, padx=10, pady=10, sticky="w")
+        account_detail_btn.append(ttk.Button(win, text="Transactions"))
+        account_detail_btn[i].grid(row=i + 1, column=1, padx=10, pady=10)
+        account_transaction_btn.append(ttk.Button(win, text="Modify"))
+        account_transaction_btn[i].grid(row=i + 1, column=2, padx=10, pady=10)
+        account_del_btn.append(
+            ttk.Button(win, text="Delete",command=lambda x=account:
+            account_delete(x, accounts_tab,account_labels + account_detail_btn + account_transaction_btn + account_del_btn)))
+        account_del_btn[i].grid(row=i + 1, column=3, padx=10, pady=10)
+        i += 1
+
+def account_view_transactions(account: tuple, win: Frame, items: list):
+    pass
+
+def account_modify(account: tuple, win: Frame, items: list):
+    pass
+
+def account_delete(account: tuple, win: Frame, items: list):
+    accounts_keys = tuple(accounts.keys())
+    print(accounts_keys, account)
+    for i in range(len(accounts)):
+        if accounts_keys[i] == account[0]:
+            accounts.pop(account[0])
+            save_accounts()
+            print_account_data(win, items)
+            break
+
+
+with open("accounts.json", "r") as f:
+    global accounts
+    accounts = json.load(f)
+
+print(accounts)
+global transactions
 transactions = pd.read_csv("transactions.csv")
 
 root = Tk()
@@ -33,5 +82,8 @@ tab_manager.select(categories_tab)
 
 accounts_tab_add_account_btn = ttk.Button(accounts_tab, text="Add Account")
 accounts_tab_add_account_btn.grid(row=0, column=0, padx=10, pady=10)
+
+print_account_data(accounts_tab)
+
 
 root.mainloop()
